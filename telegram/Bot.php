982b669c50e5f4a34a38cb2f44809d3431af655a
aethletic <gorshkov34@gmail.com>
2020-03-28 00:36:35 +0400

    [feat] добавлены новые методы 💦

--- telegram/Bot.php
+++ telegram/Bot.php
@@ -7,22 +7,53 @@ require_once __DIR__ . '/File.php';
 
 class Bot
 {
-    // system settings
+    /**
+     * Токен бота
+     */
     protected $token;
+
+    /**
+     * Базовый адрес API
+     */
     protected $base_url = 'https://api.telegram.org/bot';
 
-    // telegram settings
+    /**
+     * Массив с действиями
+     */
     protected $events = [];
-    protected $keyboards = [];
+
+    /**
+     * Массив с колбэками для инлайн-кнопок
+     */
     protected $callbacks = [];
+
+    /**
+     * Массив с клавиатурами
+     */
+    protected $keyboards = [];
+
+    /**
+     * Массив с данными о пользователе
+     */
     protected $user = [];
+
+    /**
+     * Массив с данными о новом сообщении
+     */
     protected $updates = [];
+
+    /**
+     * Объект Logger
+     */
     protected $logger;
 
-    // bot settings
+    /**
+     * Массив с настрйоками бота
+     */
     protected $config = [
         'rick.logs' => false,
         'rick.logs_dir' => '',
+        'rick.default_parse_mode' => 'markdown',
     ];
 
     public function __construct($token = null, $config = [])
@@ -38,30 +69,44 @@ class Bot
         $this->setData();
     }
 
-    public function execute($method, $params, $is_file = false)
+    public function execute($method, $params = false, $isFile = false)
     {
         $url = $this->base_url . $this->token . '/' . $method;
 
-        if ($is_file) {
-            $content_type = 'Content-Type: multipart/form-data';
+        if ($isFile) {
+            $contentType = 'Content-Type: multipart/form-data';
         } else {
-            $content_type = 'Content-Type: application/json';
+            $contentType = 'Content-Type: application/json';
             $params = json_encode($params);
         }
 
-        $ch = curl_init($url);
+        return Bot::request([
+            'method'    => 'POST',
+            'url'       => $url,
+            'params'    => $params,
+            'header'    => $contentType,
+            'return'    => 'array',
+        ]);
+    }
+
+    public function request($data)
+    {
+        $ch = curl_init($data['url']);
 
         curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [$content_type]);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [$data['header']]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        $response = json_decode(curl_exec($ch), true);
+        if (mb_strtolower($data['method']) == 'post') {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data['params']);
+        }
+
+        $response = curl_exec($ch);
 
         curl_close($ch);
 
-        return $response;
+        return mb_strtolower($data['return']) == 'array' ? json_decode($response, true) : $response;
     }
 
     public function hear($events = [], $callback)
@@ -73,18 +118,22 @@ class Bot
         foreach ($events as $event) {
             $this->events[$event] = $callback;
         }
+
+        return $this;
     }
 
     // реакция на колбэк_дату из инлайн кнопок
-    public function callback($events = [], $callback)
+    public function callback($callback_data = [], $callback)
     {
-        if (!is_array($events)) {
-            $events = [$events];
+        if (!is_array($callback_data)) {
+            $callback_data = [$callback_data];
         }
 
-        foreach ($events as $event) {
-            $this->callbacks[$event] = $callback;
+        foreach ($callback_data as $c) {
+            $this->callbacks[$c] = $callback;
         }
+
+        return $this;
     }
 
     public function say($message, $keyboard = false)
@@ -92,7 +141,7 @@ class Bot
         $params = [
             'chat_id'       => $this->user['chat_id'],
             'text'          => $this->randomMessage($message),
-            'parse_mode'    => 'markdown',
+            'parse_mode'    => $this->config['rick.default_parse_mode'],
         ];
 
         if ($keyboard) {
@@ -107,7 +156,7 @@ class Bot
         $params = [
             'chat_id'               => $this->user['chat_id'],
             'text'                  => $this->randomMessage($message),
-            'parse_mode'            => 'markdown',
+            'parse_mode'            => $this->config['rick.default_parse_mode'],
             'reply_to_message_id'   => $this->updates['message']['message_id'],
         ];
 
@@ -123,7 +172,7 @@ class Bot
         $params = [
             'chat_id'      => $this->user['chat_id'],
             'text'         => $this->randomMessage($message),
-            'parse_mode'   => 'markdown',
+            'parse_mode'   => $this->config['rick.default_parse_mode'],
             'message_id'   => $message_id,
         ];
 
@@ -139,7 +188,7 @@ class Bot
         $params = [
             'chat_id'       => $chat_id,
             'text'          => $message,
-            'parse_mode'    => 'markdown',
+            'parse_mode'    => $this->config['rick.default_parse_mode'],
         ];
 
         if ($keyboard) {
@@ -149,42 +198,6 @@ class Bot
         return $this->execute('sendMessage', $params);
     }
 
-    public function run()
-    {
-        echo 'ok';
-
-        if ($this->config['rick.logs']) {
-            // $this->logger->log($this->user);
-            $this->logger->logUpdate($this->updates);
-        }
-
-        // если поступил только колбэк то сообщения не обрабатываем
-        if (array_key_exists('callback_query', $this->updates)) {
-            $callback_data = $this->updates['callback_query']['data'];
-
-            if (!array_key_exists($callback_data, $this->callbacks)) {
-                $callback_data = '{default}';
-            }
-
-            if (strpos($this->callbacks[$callback_data], '::') == false) {
-                return $this->callbacks[$callback_data]();
-            } else {
-                return $this->callbacks[$callback_data]($this, $this->user, $this->updates);
-            }
-        }
-
-        $message = $this->user['message'];
-
-        if (!array_key_exists($message, $this->events)) {
-            $message = '{default}';
-        }
-
-        if (strpos($this->events[$message], '::') == false) {
-            return $this->events[$message]();
-        } else {
-            return $this->events[$message]($this, $this->user, $this->updates);
-        }
-    }
 
     public function randomMessage($message)
     {
@@ -208,11 +221,7 @@ class Bot
     {
         $updates = json_decode(file_get_contents('php://input'), true);
 
-        if (!is_array($updates) || count($updates) == 0) {
-            exit('Обновлений нет');
-        }
-
-        if (!array_key_exists('callback_query', $updates)) {
+        if (!$this->isCallback()) {
             $this->user['chat_id'] = $updates['message']['chat']['id'];
             $this->user['chat_name'] = trim($updates['message']['chat']['first_name'] . ' ' . $updates['message']['chat']['last_name']);
             $this->user['chat_username'] = $updates['message']['chat']['username'];
@@ -270,13 +279,15 @@ class Bot
         return $this->user;
     }
 
+    // any type
+    // 50 MB
     public function sendDocument($chat_id, $file, $message = '', $keyboard = false)
     {
         $params = [
             'chat_id' => $chat_id,
             'document' => $file,
             'caption' => $message,
-            'parse_mode' => 'markdown',
+            'parse_mode' => $this->config['rick.default_parse_mode'],
         ];
 
         if ($keyboard) {
@@ -292,7 +303,7 @@ class Bot
             'chat_id' => $chat_id,
             'photo' => $file,
             'caption' => $message,
-            'parse_mode' => 'markdown',
+            'parse_mode' => $this->config['rick.default_parse_mode'],
         ];
 
         if ($keyboard) {
@@ -300,6 +311,108 @@ class Bot
         }
 
         $this->execute('sendPhoto', $params, true);
+    }
+
+    // .OGG file encoded with OPUS (other formats may be sent as Audio or Document)
+    // 50 MB
+    public function sendVoice($chat_id, $file, $message = '', $keyboard = false)
+    {
+        $params = [
+            'chat_id' => $chat_id,
+            'voice' => $file,
+            'caption' => $message,
+            'parse_mode' => $this->config['rick.default_parse_mode'],
+        ];
+
+        if ($keyboard) {
+            $params['reply_markup'] = $keyboard;
+        }
+
+        $this->execute('sendVoice', $params, true);
+    }
+
+    // .MP3 or .M4A format
+    // 50 MB
+    public function sendAudio($chat_id, $file, $message = '', $keyboard = false)
+    {
+        $params = [
+            'chat_id' => $chat_id,
+            'audio' => $file,
+            'caption' => $message,
+            'parse_mode' => $this->config['rick.default_parse_mode'],
+        ];
+
+        if ($keyboard) {
+            $params['reply_markup'] = $keyboard;
+        }
+
+        $this->execute('sendAudio', $params, true);
+    }
+
+    // mp4 videos (other formats may be sent as Document)
+    // 50 MB
+    public function sendVideo($chat_id, $file, $message = '', $keyboard = false)
+    {
+        $params = [
+            'chat_id' => $chat_id,
+            'video' => $file,
+            'caption' => $message,
+            'parse_mode' => $this->config['rick.default_parse_mode'],
+        ];
+
+        if ($keyboard) {
+            $params['reply_markup'] = $keyboard;
+        }
+
+        $this->execute('sendVideo', $params, true);
+    }
+
+    // GIF or H.264/MPEG-4 AVC video without sound
+    // 50 MB
+    public function sendAnimation($chat_id, $file, $message = '', $keyboard = false)
+    {
+        $params = [
+            'chat_id' => $chat_id,
+            'animation' => $file,
+            'caption' => $message,
+            'parse_mode' => $this->config['rick.default_parse_mode'],
+        ];
+
+        if ($keyboard) {
+            $params['reply_markup'] = $keyboard;
+        }
+
+        $this->execute('sendAnimation', $params, true);
+    }
+
+    /**
+     * Доступные варианты действий:
+     * typing, upload_photo, record_video, upload_video, record_audio,
+     * upload_audio, upload_document, find_location, record_video_note,
+     * upload_video_note
+     */
+    public function action($action)
+    {
+        $params = [
+            'chat_id' => $this->user['chat_id'],
+            'action' => $action,
+        ];
+
+        $this->execute('sendChatAction', $params);
+
+        return $this;
+    }
+
+    public function sendAction($chat_id, $action)
+    {
+        $params = [
+            'chat_id' => $chat_id,
+            'action' => $action,
+        ];
+
+        $this->execute('sendChatAction', $params);
+
+        return $this;
     }
 
     public function inline($inline)
@@ -313,7 +426,7 @@ class Bot
 
     public function notify($message)
     {
-        if (!array_key_exists('callback_query', $this->updates)) {
+        if (!$this->isCallback()) {
             return false;
         }
 
@@ -321,5 +434,70 @@ class Bot
             'callback_query_id' => $this->updates['callback_query']['id'],
             'text' => $message,
         ]);
+    }
+
+    public function isCallback()
+    {
+        if (!is_array($this->updates)) {
+            return;
+        }
+
+        return array_key_exists('callback_query', $this->updates);
+    }
+
+    public function run()
+    {
+
+        if ($this->updates == '') {
+            $this->json(['message' => 'Обновлений нет']);
+            return;
+        }
+
+        echo 'ok';
+
+        if ($this->config['rick.logs']) {
+            $this->logger->log($this->user, $this->updates);
+        }
+
+        // если поступил только колбэк то сообщения не обрабатываем
+        if ($this->isCallback()) {
+            $callback_data = $this->updates['callback_query']['data'];
+
+            if (!array_key_exists($callback_data, $this->callbacks)) {
+                $callback_data = '{default}';
+            }
+
+            if (strpos($this->callbacks[$callback_data], '::') == false) {
+                return $this->callbacks[$callback_data]();
+            } else {
+                return $this->callbacks[$callback_data]($this, $this->user, $this->updates);
+            }
+        }
+
+        $message = $this->user['message'];
+
+        if (!array_key_exists($message, $this->events)) {
+            $message = '{default}';
+        }
+
+        if (strpos($this->events[$message], '::') == false) {
+            return $this->events[$message]();
+        } else {
+            return $this->events[$message]($this, $this->user, $this->updates);
+        }
+    }
+
+    public function json($array)
+    {
+        echo '<pre>';
+        echo json_encode($array, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        echo '</pre>';
+    }
+
+    public function debug($message)
+    {
+        echo '<pre>';
+        echo print_r($message);
+        echo '</pre>';
     }
 }
